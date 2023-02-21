c4b124bd338c424a36517a0f81a1567d5a4a3e5b
sharan9611 <117658090+sharan9611@users.noreply.github.com>
2023-02-21 12:03:31 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
-package main
-
-import (
-	"fmt"
-	"log"
-	"os"
-)
-
-func main() {
-	log.Println("Hello, go!")
-	// Attempting to write to other directory other then the
-	// allowed ones in the Dockerfile will result in the
-	// following error:
-	// 2019/01/22 07:51:37 open /hello.txt: permission denied
-	f, err := os.Create("/tmp/hello.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	_, err = f.Write([]byte("hello world"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("file written")
-}
+package main 
+import "fmt"
+func main () {
+     fmt.Println("Hello from SHARAN!!!")
+}	 
